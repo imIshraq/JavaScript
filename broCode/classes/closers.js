2a2ad21b911bbc6613ed17ca48a5ea88727cd124
imIshraq <ishraq1514@gmail.com>
2024-03-13 22:58:18 +0530

    had to practise these methods again again to get perfectlyy

--- broCode/classes/closers.js
+++ broCode/classes/closers.js
@@ -27,4 +27,29 @@ counter.increment();
 counter.increment();
 
 console.log(`the current count is ${counter.getCount()}`);
+console.log(`the current count is ${counter.getCount()}`);
 
+function createGame(){
+    let score =0;
+    function increaseScore(points){
+        score += points;
+        console.log(`+${points}pts`);
+    }  
+
+    function decreaseScore(points){
+        score -= points;
+        console.log(`-${points}pts`);
+    }
+
+
+    function getScore(){
+       return score;
+    }
+    return {increaseScore,decreaseScore,getScore }; 
+}
+
+const game = createGame(); 
+
+game.increaseScore(3);
+game.decreaseScore(6);
+console.log(`The final score is${game.getScore()}pts`);

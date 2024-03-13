9e6016d9131a599799574cc40130eac4c44b8fc8
imIshraq <ishraq1514@gmail.com>
2024-03-13 14:41:35 +0530

    if shuffling cards make easy look

---
+++ broCode/classes/shufflingCard.js
@@ -0,0 +1,12 @@
+// shuffle the elements of an array
+const cards =["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
+
+
+console.log(cards);
+function shuffle(){
+    for (let i= array.length-1; i>0 ;i--){
+        const random =Math.floor(Math.random()*i+1);
+
+        [array[i],array[random]]= [array[random],array[i]];
+    }
+}
